38d853a3805b9ca94a6fb06c46c483a0ea64d334
Gus <ychcom@naver.com>
2025-09-29 16:48:20 +0900

    init main
    초기 github

--- Visiom/ContentView.swift
+++ Visiom/ContentView.swift
@@ -16,7 +16,6 @@ struct ContentView: View {
             Model3D(named: "Scene", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
-            Text("Hello, world!")
 
             ToggleImmersiveSpaceButton()
         }
