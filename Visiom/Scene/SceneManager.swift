8b22c77f21019d8602b64bde4fdb380d42883701
Gus <ychcom@naver.com>
2025-10-20 21:43:32 +0900

    markersVisible, toggleMarkers 추가
    markersVisible : 마커 상태값
    toggleMarkers : markersVisible를 토글하는 함수

---
+++ Visiom/Scene/SceneManager.swift
@@ -0,0 +1,18 @@
+//
+//  SceneManager.swift
+//  Visiom
+//
+//  Created by 윤창현 on 10/20/25.
+//
+
+import SwiftUI
+
+struct SceneManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SceneManager()
+}
