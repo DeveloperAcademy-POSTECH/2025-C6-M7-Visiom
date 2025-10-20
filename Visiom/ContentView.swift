12e7020a3c9b9b414f626fde37aea871eb95d1d9
Gus <ychcom@naver.com>
2025-10-20 21:45:13 +0900

    사진윈도우, 툴바 추가
    이멀시브 상태에서도 마커조정가능한 윈도우

--- Visiom/ContentView.swift
+++ Visiom/ContentView.swift
@@ -6,20 +6,53 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
+ㅅ
+// TODO : extension 파일로 옮길 예정
+extension Image {
+  init(resource name: String, ofType type: String) {
+    guard let path = Bundle.main.path(forResource: name, ofType: type),
+          let image = UIImage(contentsOfFile: path) else {
+      self.init(name)
+      return
+    }
+    self.init(uiImage: image)
+  }
+}
 
 struct ContentView: View {
+    @Environment(AppModel.self) var appModel
 
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-
-            ToggleImmersiveSpaceButton()
+        VStack(alignment: .trailing) {
+            if appModel.immersiveSpaceState == .open {
+                EmptyView()
+            } else {
+                Image(resource: "environment", ofType: "png")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .glassBackgroundEffect()
+            }
         }
-        .padding()
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomOrnament) {
+                VStack(spacing: 12) {
+                    ToggleImmersiveSpaceButton()
+                    
+                    if appModel.immersiveSpaceState == .open {
+                        Button(action: {
+                            appModel.toggleMarkers()
+                        }) {
+                            Label(appModel.markersVisible ? "마커 숨기기" : "마커 보이기",
+                                  systemImage: appModel.markersVisible ? "eye.slash.fill" : "eye.fill")
+                        }
+                    }
+                    
+                    Text("바닥의 파란색 원을 탭하여 텔레포트")
+                        .font(.caption2)
+                        .foregroundStyle(.secondary)
+                }
+            }
+        }
     }
 }
 
