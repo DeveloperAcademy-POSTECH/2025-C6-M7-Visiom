4ee82851f42f9e0081134aecd04fea2251d6f2ff
Gus <ychcom@naver.com>
2025-10-21 23:53:07 +0900

    ✨Add. 텍스트모델 기능 구현, 모듈화로 분리
    loadModelWithTextField : 텍스트필드 붙일모델 로딩
    
    TextFieldAttachmentView : 텍스트필드 attachment 컴포넌트

---
+++ Visiom/Helpers/ModelLoader.swift
@@ -0,0 +1,18 @@
+//
+//  ModelLoader.swift
+//  Visiom
+//
+//  Created by 윤창현 on 10/21/25.
+//
+
+import SwiftUI
+
+struct ModelLoader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelLoader()
+}
