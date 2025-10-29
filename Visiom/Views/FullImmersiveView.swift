ce71501da6433de8bc6e0f96ba95ef3efb7ef1c2
HwangJeha <155410554+Jeha0101@users.noreply.github.com>
2025-10-29 23:10:51 +0900

    ⚡️Feat. Photo 공간 버튼 선택시 PhotoCollection 생성
    Why: Photo 공간 버튼 기능 부여
    How: PhotoCollectionWindow를 열기 위한 환경값 추가
    공간 버튼 생성시 새로운 collection을 생성
    공간 버튼 배치 완료시 UUID를 이용해서 collection 연결
    공간 버튼 클릭시 openWindow 함수 호출

--- Visiom/Views/FullImmersiveView.swift
+++ Visiom/Views/FullImmersiveView.swift
@@ -17,37 +17,41 @@ struct WorldAnchorEntityData {
 
 struct FullImmersiveView: View {
     @Environment(AppModel.self) var appModel
-
+    @Environment(CollectionStore.self) var collectionStore
+    @Environment(\.openWindow) private var openWindow
+    
     private static let session = ARKitSession()
     private static let handTracking = HandTrackingProvider()
     private static let worldTracking = WorldTrackingProvider()
-
+    
     @State private var root = Entity()
-
     @State private var worldAnchorEntityData: [UUID: WorldAnchorEntityData] =
-        [:]
+    [:]
     // 임시 객체 상태일 때 타입이랑 uuid를 저장하는 친구
     @State private var tempItemType: [UUID: UserControlBar] = [:]
-
+    
     @State private var isPlaced = false
     @State private var currentItem: ModelEntity? = nil
     @State private var currentItemType: UserControlBar? = nil
-
+    
+    @State private var anchorToCollection: [UUID: UUID] = [:]
+    @State private var pendingCollectionIdForNextAnchor: UUID? = nil
+    
     let ball: ModelEntity = {
         let ball = ModelEntity(
             mesh: .generateSphere(radius: 0.05),
             materials: [SimpleMaterial(color: .cyan, isMetallic: false)]
         )
-
+        
         let collision = CollisionComponent(shapes: [
             .generateSphere(radius: 0.05)
         ])
         let input = InputTargetComponent()  // 상호작용할 수 있는 객체임을 표시해주는 컴포넌트
         ball.components.set([collision, input])
-
+        
         return ball
     }()
-
+    
     let box: ModelEntity = {
         let box = ModelEntity(
             mesh: .generateBox(size: 0.1),
@@ -60,7 +64,7 @@ struct FullImmersiveView: View {
         box.components.set([collision, input])
         return box
     }()
-
+    
     var body: some View {
         VStack {
             // TO DO: UserControlView랑 합치기
@@ -77,7 +81,7 @@ struct FullImmersiveView: View {
         }
         .allowsHitTesting(!isPlaced)
         .disabled(isPlaced)
-
+        
         RealityView { content in
             content.add(root)
             // 씬 갈아끼기
@@ -88,19 +92,19 @@ struct FullImmersiveView: View {
                 immersiveContentEntity.generateCollisionShapes(recursive: true)
                 root.addChild(immersiveContentEntity)
             }
-
+            
             let headAnchor = AnchorEntity(.head)
             content.add(headAnchor)
-
+            
             let card = ViewAttachmentEntity()
             card.attachment = ViewAttachmentComponent(
                 rootView: UserControlView()
                     .environment(appModel)
             )
             card.position = [0, -0.3, -0.9]
-
+            
             headAnchor.addChild(card)
-
+            
         } update: { content in
             for (_, data) in worldAnchorEntityData {
                 if !content.entities.contains(data.entity) {
@@ -117,7 +121,7 @@ struct FullImmersiveView: View {
                     let targetEntity = value.entity
                     let anchorUUIDString = targetEntity.name
                     guard !anchorUUIDString.isEmpty,
-                        let anchorUUID = UUID(uuidString: anchorUUIDString)
+                          let anchorUUID = UUID(uuidString: anchorUUIDString)
                     else {
                         print("Tapped entity has no valid UUID name.")
                         return
@@ -125,7 +129,7 @@ struct FullImmersiveView: View {
                     if let itemType = tempItemType[anchorUUID] {
                         switch itemType {
                         case .photo:
-                            tapPhotoButton()
+                            tapPhotoButton(anchorUUID)
                         case .memo:
                             tapMemoButton()
                         }
@@ -139,12 +143,12 @@ struct FullImmersiveView: View {
                 .targetedToAnyEntity()
                 .onEnded { value in
                     let targetEntity = value.entity
-
+                    
                     guard let anchorUUID = UUID(uuidString: targetEntity.name)
                     else {
                         return
                     }
-
+                    
                     Task {
                         await removeWorldAnchor(by: anchorUUID)
                     }
@@ -158,7 +162,7 @@ struct FullImmersiveView: View {
         }
         .task(id: isPlaced) {
             guard isPlaced,
-                let currentItem
+                  let currentItem
             else { return }
             await trackingHand(currentItem)
         }
@@ -171,14 +175,14 @@ struct FullImmersiveView: View {
         //            }
         //        }
     }
-
+    
     private static func startARSession() async {
         guard HandTrackingProvider.isSupported
         else {
             print("error: 핸드 트래킹이 안됨")
             return
         }
-
+        
         guard WorldTrackingProvider.isSupported
         else {
             print("error: 월드 트래킹이 안됨")
@@ -190,14 +194,14 @@ struct FullImmersiveView: View {
             print("AR session falied")
         }
     }
-
+    
     private func observeUpdate() async {
         do {
             for await update in Self.worldTracking.anchorUpdates {
                 switch update.event {
                 case .added:
                     let subjectClone: ModelEntity
-
+                    
                     if tempItemType[update.anchor.id] == .photo {
                         subjectClone = ball.clone(recursive: true)
                     } else {
@@ -208,17 +212,17 @@ struct FullImmersiveView: View {
                         update.anchor.originFromAnchorTransform,
                         relativeTo: nil  // 월드 좌표 기준
                     )
-
+                    
                     worldAnchorEntityData[update.anchor.id] =
-                        WorldAnchorEntityData(
-                            anchor: update.anchor,
-                            entity: subjectClone
-                        )
-
+                    WorldAnchorEntityData(
+                        anchor: update.anchor,
+                        entity: subjectClone
+                    )
+                    
                     print("🟢 Anchor added \(update.anchor.id)")
-
+                    
                 case .updated:
-
+                    
                     if var updateAnchor = worldAnchorEntityData[
                         update.anchor.id
                     ] {
@@ -226,13 +230,13 @@ struct FullImmersiveView: View {
                             update.anchor.originFromAnchorTransform,
                             relativeTo: nil
                         )
-
+                        
                         updateAnchor.anchor = update.anchor
-
+                        
                         worldAnchorEntityData[update.anchor.id] = updateAnchor
                     }
                     print("🔵 Anchor updated \(update.anchor.id)")
-
+                    
                 case .removed:
                     if let removeAnchor = worldAnchorEntityData.removeValue(
                         forKey: update.anchor.id
@@ -244,78 +248,82 @@ struct FullImmersiveView: View {
             }
         }
     }
-
+    
     private func makePlacement(type: UserControlBar) {
         guard !isPlaced else { return }
-
+        
         // 손을 따라다니는 임시 객체를 생성
         let tempObject: ModelEntity
-
+        
         if type == .photo {
             tempObject = ball.clone(recursive: true)
+            
+            let newCol = collectionStore.createCollection()
+            collectionStore.renameCollection(newCol.id, to: newCol.id.uuidString)
+            pendingCollectionIdForNextAnchor = newCol.id
         } else {
             tempObject = box.clone(recursive: true)
         }
-
+        
         root.addChild(tempObject)
         print("객체 생성 완료")
         self.currentItem = tempObject
         self.currentItemType = type
         self.isPlaced = true
     }
-
+    
     private func trackingHand(_ currentBall: ModelEntity) async {
         // 직전 상태 저장
         var tapDetectedLastFrame = true
-
+        
         // 계속 핸드트래킹의 업데이트 받기
         for await update in Self.handTracking.anchorUpdates {
             guard isPlaced else { return }
-
+            
             guard update.anchor.chirality == .right,
-                update.anchor.isTracked,
-                let skeleton = update.anchor.handSkeleton
+                  update.anchor.isTracked,
+                  let skeleton = update.anchor.handSkeleton
             else { continue }
-
+            
             // 검지 끝 위치 가져오기
             let indexTipJoint = skeleton.joint(.indexFingerTip)
             let originFromWorld = update.anchor.originFromAnchorTransform
             let indexTipTransform =
-                originFromWorld * indexTipJoint.anchorFromJointTransform
+            originFromWorld * indexTipJoint.anchorFromJointTransform
             let indexTipPosition = simd_make_float3(indexTipTransform.columns.3)
-
+            
             // 객체 위치를 검지 끝 위치로 실시간 업데이트
             await MainActor.run {
                 currentBall.setPosition(indexTipPosition, relativeTo: nil)
-
+                
             }
-
+            
             // 탭 감지
             // 엄지끝 위치 가져오기
             let thumbTipJoint = skeleton.joint(.thumbTip)
             let thumbTipTransform =
-                originFromWorld * thumbTipJoint.anchorFromJointTransform
+            originFromWorld * thumbTipJoint.anchorFromJointTransform
             let thumbTipPosition = simd_make_float3(thumbTipTransform.columns.3)
-
+            
             // 엄지끝~검지끝 사이의 거리 계산
             let distance = simd_distance(indexTipPosition, thumbTipPosition)
             let tapDetected = distance < 0.02  // 2cm 이내면 탭으로 인식
-
+            
             // 탭 감지 + 직전 상태는 탭 상태가 아니어야 함
             if tapDetected && !tapDetectedLastFrame {
                 await MainActor.run {
                     print("placement")
-
+                    
                     // ball의 최종 위치(월드 좌표) 가져와
                     let finalPosition = currentBall.transformMatrix(
                         relativeTo: nil
                     )
-
+                    
                     currentBall.removeFromParent()
-
+                    
                     self.isPlaced = false
                     self.currentItem = nil
-
+                    
                     // 별도 Task에서 월드 앵커를 생성(MainActor에서 네트워킹/ARKit 작업을 하면 UI가 멈출 수 있음(?))
                     Task {
                         do {
@@ -330,6 +338,13 @@ struct FullImmersiveView: View {
                                 if let itemType = self.currentItemType {
                                     tempItemType[anchor.id] = itemType
                                 }
+                                
+                                // 앵커ID와 컬렉션 ID를 연결함
+                                if let colId =
+                                    pendingCollectionIdForNextAnchor {
+                                    anchorToCollection[anchor.id] = colId
+                                    pendingCollectionIdForNextAnchor = nil
+                                }
                             }
                         } catch {
                             print("월드 앵커 추가 failed")
@@ -340,7 +355,7 @@ struct FullImmersiveView: View {
             tapDetectedLastFrame = tapDetected
         }
     }
-
+    
     private func removeWorldAnchor(by id: UUID) async {
         do {
             if let anchorToRemove = worldAnchorEntityData[id]?.anchor {
@@ -353,9 +368,16 @@ struct FullImmersiveView: View {
             print("error: \(error)")
         }
     }
-
-    private func tapPhotoButton() {
+    
+    private func tapPhotoButton(_ anchorUUID: UUID) {
         print("ball 클릭 ")
+        guard let colId = anchorToCollection[anchorUUID] else {
+            print("No collection mapped for anchor \(anchorUUID)")
+            return
+        }
+        // PhotoCollectionWindow 열기
+        openWindow(id: "PhotoCollectionWindow", value: colId)
+        print("Opened collection window for \(colId)")
     }
     private func tapMemoButton() {
         print("box 클릭 ")
