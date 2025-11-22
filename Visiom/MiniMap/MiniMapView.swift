31dd29f05ed268b0057c4a1810d13a16e1b76645
Gus <ychcom@naver.com>
2025-11-22 17:11:10 +0900

    미니맵 사용하지 않은 버튼 삭제
    미니맵 사용하지 않은 버튼 삭제

--- Visiom/MiniMap/MiniMapView.swift
+++ Visiom/MiniMap/MiniMapView.swift
@@ -14,26 +14,6 @@ struct MiniMapView: View {
     @Environment(MiniMapManager.self) private var miniMapManager
     
     var body: some View {
-      
-        HStack(spacing: 20) {
-                    Button(action: {
-                        miniMapManager.isRotated = true
-                    }) {
-                        Label("90도 회전", systemImage: "rotate.right")
-                            .padding()
-                            .background(Color.blue.opacity(0.2))
-                            .cornerRadius(10)
-                    }
-                    
-                    Button(action: {
-                        miniMapManager.isRotated = false
-                    }) {
-                        Label("원래대로", systemImage: "arrow.counterclockwise")
-                            .padding()
-                            .background(Color.green.opacity(0.2))
-                            .cornerRadius(10)
-                    }
-                }
         RealityView { content in
             await miniMapManager.setupMiniScene(content: content)
         } update: { content in
