62bb9f2a17e3ec01453414b35221d6d832219223
jiwonpark831 <jiwonpark831@gmail.com>
2025-11-24 11:02:59 +0900

    UI.photo 호버 이팩트 추가

--- Visiom/Features/Photo/PhotoViews/ImageGalleryView.swift
+++ Visiom/Features/Photo/PhotoViews/ImageGalleryView.swift
@@ -39,7 +39,8 @@ struct ImageGalleryView: View {
                                 cornerRadius: cornerRadius,
                                 isSelected: idx == selectedIndex,
                                 size: thumbnailSize
-                            ).hoverEffect(.lift)
+                            )
+                            .hoverEffect(.lift)
                         }
                         .overlay(alignment: .bottomLeading) {
                             FilenameBadge(url: url)
