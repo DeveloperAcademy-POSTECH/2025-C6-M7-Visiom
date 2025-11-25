e034d6a64809aae6024db63f3457b466ae825fe3
jiwonpark831 <jiwonpark831@gmail.com>
2025-11-25 10:42:09 +0900

    UI.timeline 버튼 UI 수정

--- Visiom/Features/Timeline/TimelineViews/TimelineBoardView.swift
+++ Visiom/Features/Timeline/TimelineViews/TimelineBoardView.swift
@@ -127,10 +127,10 @@ struct TimelineBoardView: View {
                             .padding(.leading, 6)
                             .padding(.trailing, 10)
                     }
-                    .frame(width: 93, height: 36)
+                    .frame(width: 90, height: 43)
                     .background(.ultraThickMaterial)
                     .clipShape(
-                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
                     )
                     .buttonStyle(.plain)
                     .onTapGesture {
@@ -145,13 +145,10 @@ struct TimelineBoardView: View {
                         ),
                         displayedComponents: [.hourAndMinute]
                     )
-                    .background(.ultraThickMaterial)
+                    .foregroundStyle(.ultraThickMaterial)
                     .labelsHidden()
                     .environment(\.locale, Locale(identifier: "en_GB"))
                     .frame(width: 93, height: 36)
-                    .clipShape(
-                        RoundedRectangle(cornerRadius: 30, style: .continuous)
-                    )
                 }
             }
             .padding(.horizontal, 24)
